3b908cc5dff029d5e6e0b4ebd4efb8ed4cad6211
shafamirazka <shafamiraz@gmail.com>
2024-09-18 13:53:21 +0700

    tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class MoodEntry(models.Model):
@@ -8,13 +9,8 @@ class MoodEntry(models.Model):
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     @property
     def is_mood_strong(self):
         return self.mood_intensity > 5
-    
-
-# models.Model adalah kelas dasar yang digunakan untuk mendefinisikan model dalam Django.
-# MoodEntry adalah nama model yang kamu definisikan.
-# mood, time, feelings, dan mood_intensity adalah atribut atau field pada model. Setiap field memiliki tipe data yang sesuai seperti CharField, DateField, IntegerField, dan TextField.
-# Decorator @property digunakan untuk menambahkan atribut read-only yang tidak disimpan ke dalam basis data, namun merupakan hasil derivasi/perhitungan dari atribut lain. Dalam hal ini, fungsi is_mood_strong() yang diberikan decorator @property mengukur apakah mood pengguna pada saat itu tergolong "kuat" berdasarkan mood_intensity.
--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3b3)&4(m2&u+s!#p%g36)!0g%cw5l8j5h(3g7a=&v+rkl11&#a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shafa-amira31-mentalhealthtrackerr.pbp.cs.ui.ac.id"]
 
